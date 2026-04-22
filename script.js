a5e979c80914c7b9a1a7e9af8ed5ce7c874a38b6
ibnuluthfi291-cpu <ibnuluthfi291@gmail.com>
2026-04-22 19:21:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPage(page){
+document.getElementById("home").style.display="none";
+document.getElementById("katalog").style.display="none";
+
+```
+document.getElementById(page).style.display="block";
+```
+
+}
